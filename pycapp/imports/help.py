e081091deb5bd4070ecd66813f0f1c6a752df8bb
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2025-12-11 17:06:38 +1100

    Create help.py with various help topics
    Add documentation

---
+++ pycapp/imports/help.py
@@ -0,0 +1,6 @@
+def replCmd():
+  print("""Using the REPL
+REPL stands for read-evaluate-print-loop. It refers to the way it works, by reading the command, doing the action, printing an output and doing that for every command. The REPL is a terminal where you can type in commands and they execute instantly.
+To use a command, type it in the format of module.command(configuration). For example, to print 'Hello, World!' onto the screen, type pyc.writeline("Hello, World!")
+
+For more help, either type help.topiclist() or go to the PyC wiki on GitHub.""")
